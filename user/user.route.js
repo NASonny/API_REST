fc374cd354c783e0d036447605ac1266719388c2
Sonny Naidja <133282829+NASonny@users.noreply.github.com>
2025-09-25 10:55:04 +0200

    modify user.route

--- user/user.route.js
+++ user/user.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../user/user.controller');
 
-router.post('/register', userController.register);
+router.post('/register', userController.signin);
 router.post('/login', userController.login);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
