8400e9dabc5b84ebdb6b12005c8e17fad1c525a7
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-09-25 12:23:52 +0200

    s
    s

--- comment/comment.route.js
+++ comment/comment.route.js
@@ -4,11 +4,11 @@ const commentController = require("./comment.controller")
 const limiter = require('../middleware/ratelimiter.middleware.js');
 const auth = require("../auth/auth.middleware.js")
 
-router.post('/posts/:id/comments',auth, limiter,commentController.createcom)
+router.post('/post/:id/comments',auth, limiter,commentController.createcom)
 
-router.get('/posts/:id/comments',commentController.getById)
+router.get('/post/:id/comments',commentController.getById)
 
-router.put('/posts/:id',auth, limiter, commentController.update)
+router.put('/post/:id',auth, limiter, commentController.update)
 
 router.delete('/comments/:id', auth, limiter, commentController.delete)
 
