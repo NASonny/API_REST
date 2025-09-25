8f832ab9216d39b52dcd058a22d02c23f8287042
Shogaro <hennebellelucas4@gmail.com>
2025-09-25 10:10:12 +0200

    Route Post

---
+++ post/checkPost.middleware.js
@@ -0,0 +1,16 @@
+const Post = require('../../post/post.model')
+
+const checkPost = async (req, res, next) => {
+    let post = await Post.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+    if (post) {
+        next();
+    } else {
+        res.status(404).json({ error: "Le post n'existe pas" });
+    }
+}
+
+module.exports = checkPost;
---
+++ post/post.route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('./post.controller');
+const checkPost = require("./checkPost.middleware.js")
+const limiter = require('../middleware/ratelimiter.middleware.js');
+const auth = require("../auth/auth.middleware.js")
+const multerMiddleware = require('../middleware/multer.middleware');
+
+//Récupération
+router.get('/', limiter, postController.GetAll);
+router.get('/:id', limiter, postController.GetById);
+
+//Création
+router.post('/', auth, multerMiddleware, postController.Create);
+
+//Modification
+router.put('/:id', auth, postController.Update);
+
+//Suppression
+router.delete('/:id', auth, checkPost, postController.Delete);
+
+module.exports = router;
