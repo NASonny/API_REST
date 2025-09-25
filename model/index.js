145488aad446254f24d1b9890128c9cfa65ecabd
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-09-25 11:04:48 +0200

    app
    app

--- model/index.js
+++ model/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const {Sequelize, Model} = require("sequelize");
 const db = new Sequelize(process.env.DBNAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
 })
 
 db.authenticate()
