2de1cd99a11b6afd76d8c5a7756792e074766196
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-09-25 10:35:38 +0200

    comment
    comment

--- comment/comment.controller.js
+++ comment/comment.controller.js
@@ -1,14 +1,58 @@
-const Comment = require("comment.model")
+const Comment = require("./comment.model")
 
-exports.createcom = async(req, res, next) => {
-    try{
-        let comments = await Comment.createcom({
+exports.createcom = async (req, res, next) => {
+    try {
+        let comments = await Comment.create({
             comment: req.body.comment
         });
         res.status(201).json(comments)
-    }catch(e){
-        res.status(400).json({error: e});
+    } catch (e) {
+        res.status(400).json({ error: e });
     }
 }
 
-exports
+exports.getById = async (req, res) => {
+    try {
+        let comments = await Comment.findAll({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(comments)
+    } catch (e) {
+        res.status(400).json({ error: "impossible de récupérer les posts" })
+    }
+}
+
+exports.update = async (req, res, next) => {
+    try {
+        let comments = await Comment.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (req.token.id !== comments.userId) {
+            return res.status(403).json({ error: "Vous n'avez pas les autorisations pour modifier le post" })
+        }
+        if (req.body.comment) {
+            comments.comment = req.body.comment;
+        }
+        await comments.save();
+        res.status(201).json(comments);
+    } catch (e) {
+        res.status(400).json({ error: "Impossible de modifier ce commentaire" })
+    }
+}
+
+exports.delete = async (req, res, next) => {
+    try {
+        let comments = await Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(201).json(comments)
+    } catch (e) {
+        res.status(400).json({ error: "Ce compte existe déjà." });
+    }
+}
